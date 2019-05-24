9fbb1e9515eedac6cae695c5b7fe141b8d99e9e1
rossdrew <thek1ng13@hotmail.com>
2019-05-24 13:48:40 +0100

    Moving to 1 read per minute and outputting findings to console

--- read_cycle.py
+++ read_cycle.py
@@ -12,7 +12,6 @@ def get_readings():
     humidity, temp = Adafruit_DHT.read_retry(sensor, pin)
 
     if humidity is not None and temp is not None:
-        # TODO record
         return humidity, temp
     else:
         print('ERROR: Failure retrieving Humidity/Temp')
@@ -22,9 +21,9 @@ def get_readings():
 with open('data.csv', mode='a+') as data_file:
     csv_file = csv.writer(data_file, delimiter=',')
     while True:
-        # sleep(60)
         time = datetime.datetime.now()
         h, t = get_readings()
-        print('Temp={0:0.1f}*  Humidity={1:0.1f}%'.format(t, h))
+        print('@{0}: Temp={1:0.1f}*  Humidity={2:0.1f}%'.format(time, t, h))
         csv_file.writerow([time, t, h])
+        sleep(60)
 
