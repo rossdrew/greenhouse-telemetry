e59b78fa2dd1187d6879d6eb3c202aaa78d52d09
rossdrew <thek1ng13@hotmail.com>
2019-05-26 11:46:41 +0100

    Documentation improvements

--- read_cycle.py
+++ read_cycle.py
@@ -10,6 +10,9 @@ pin = 4
 
 
 def get_readings():
+    '''
+    :return: (humidity_reading, temperature_reading)
+    '''
     humidity, temp = Adafruit_DHT.read_retry(sensor, pin)
 
     if humidity is not None and temp is not None:
@@ -18,8 +21,10 @@ def get_readings():
         print('ERROR: Failure retrieving Humidity/Temp')
 
 
-def record_reading_to_db(time,h,t):
-    # We are assuming that the database and table are there
+def record_reading_to_db(humidity_reading, time_reading):
+    '''
+    Record the humidity_reading and time_reading for the given time in a time series database
+    '''
     dbClient = InfluxDBClient('localhost', 8086, 'root', 'root', 'TelemetryHistory')
     record_entry = [
         {
@@ -31,8 +36,8 @@ def record_reading_to_db(time,h,t):
             },
             #"time": "2009-11-10T23:00:00Z",
             "fields": {
-                "temp": t,
-                "humidity": h
+                "temp": time_reading,
+                "humidity": humidity_reading
             }
         }
     ]
@@ -46,15 +51,20 @@ def record_reading_to_db(time,h,t):
     dbClient.close()
 
 
-def record_reading_to_file(time,h,t):
-    # Record Loop
+def record_reading_to_file(time, humidity_reading, time_reading):
+    '''
+    Record the humidity_reading and time_reading for the given time in a local file
+    '''
     with open('data.csv', mode='a+') as data_file:
         csv_file = csv.writer(data_file, delimiter=',')
-        print('@{0}, Saved: Temp={1:0.1f}*  Humidity={2:0.1f}%'.format(time, t, h))
-        csv_file.writerow([time, t, h])
+        print('@{0}, Saved: Temp={1:0.1f}*  Humidity={2:0.1f}%'.format(time, time_reading, humidity_reading))
+        csv_file.writerow([time, time_reading, humidity_reading])
 
 
 while True:
+    '''
+    Main program loop
+    '''
     time = datetime.datetime.now()
     h, t = get_readings()
     print('Read @{0}: Temp={1:0.1f}*  Humidity={2:0.1f}%'.format(time, t, h))
