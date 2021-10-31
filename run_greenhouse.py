0f83e810813fbd1b8f12c44eb2438799525a013f
rossdrew <thek1ng13@hotmail.com>
2021-10-31 13:43:48 +0000

    Code tidy up

--- run_greenhouse.py
+++ run_greenhouse.py
@@ -20,6 +20,7 @@ SPI_PORT = 0
 SPI_DEVICE = 0
 mcp = Adafruit_MCP3008.MCP3008(spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE))
 
+# Handler for Ctrl+C halt
 def signal_hander(sig, frame):
 	print('\nHard exit time, closing connections...')
 	print('1/3 Forcing sunset...')
@@ -30,41 +31,6 @@ def signal_hander(sig, frame):
 	GPIO.cleanup()
 	sys.exit(0)
 
-def test_run():
-	now = datetime.now()
-
-	current_time = now.strftime("%H:%M:%S")
-	print('Starting test at {}'.format((current_time)))
-
-	print("Testing daylight...")
-	for x in range(0,5):
-		GPIO.output(light_channel, True)
-		print("on")
-		time.sleep(1)
-		GPIO.output(light_channel, False)
-		print("off")
-		time.sleep(1)
-		
-	print("Testing rain...")
-	for x in range(0,5):
-		GPIO.output(water_channel, True)
-		print("on")
-		time.sleep(1)
-		GPIO.output(water_channel, False)
-		print("off")
-		time.sleep(1)
-		
-	print("Testing moisture sensor...")
-	values = [0]*8
-	print('| {0:>4} | {1:>4} | {2:>4} | {3:>4} | {4:>4} | {5:>4} | {6:>4} | {7:>4} |'.format(*range(8)))
-	for x in range(0,0):
-		for i in range(8):
-			values[i] = mcp.read_adc(i)
-			print('| {0:>4} | {1:>4} | {2:>4} | {3:>4} | {4:>4} | {5:>4} | {6:>4} | {7:>4} |'.format(*values))
-		time.sleep(0.5)
-		
-	GPIO.cleanup()
-
 sunrise = dt_time(8,30)
 sunset = dt_time(20,00)
 
@@ -72,20 +38,22 @@ signal.signal(signal.SIGINT, signal_hander)
 
 now = datetime.now()
 current_time = now.strftime("%H:%M:%S")
-print('Starting life cycle at {}'.format((current_time)))
+print('Starting life cycle at {}'.format(current_time))
+
 light_on = False
 water_flowing = False
+
 while True:
 	now = datetime.now()
 	current_time = now.strftime("%H:%M:%S")
 	
-	if now.time() > sunrise and now.time() < sunset:
+	if sunrise < now.time() < sunset:
 		if not light_on:
-			print('Sun up at {}'.format((current_time)))
+			print('Sun up at {}'.format(current_time))
 			GPIO.output(light_channel, True)
 			light_on = True
 	elif light_on:
-		print('Sun down at {}'.format((current_time)))
+		print('Sun down at {}'.format(current_time))
 		GPIO.output(light_channel, False)
 		light_on = False
 		
